63f080fbe27292ed496ae459519471f3eafb3b53
davidgohel <david.gohel@ardata.fr>
2018-11-22 00:08:02 +0100

    add noam work

---
+++ R/dml.R
@@ -0,0 +1,145 @@
+#' Make A DrawingML plotting object to print in Powerpoint
+#'
+#' Only Powerpoint outputs currently supported. Pandoc >=2.4 is required.
+#' Currently plots with rasters are not supported.
+#'
+#' @param code plotting instructions
+#' @param ggobj ggplot object to print. argument code will be ignored if this
+#'   argument is supplied.
+#' @param layout either a character indicating layout type: "default",
+#'   "left_col", "right_col", "full_slide", or a list with elements "width",
+#'   "height", "left", and "top" in inches. Layouts are drawn from the
+#'   powerpoint template used for the R Markdown document.
+#' @param bg,fonts,pointsize,editable Parameters passed to \code{\link{dml_pptx}}
+#' @export
+#' @importFrom rvg dml_pptx
+dml <- function(code, ggobj = NULL, layout = "default",
+                bg = "white", fonts = list(), pointsize = 12, editable = TRUE) {
+  out <- list()
+  out$code <- substitute(code)
+  out$ggobj <- ggobj
+  if (!(layout %in% c("default", "left_col", "right_col", "full_slide") ||
+        (inherits(layout, "list") &&
+         setequal(names(layout), c("width", "height", "left", "top"))))) {
+    stop("Invalid layout value")
+  }
+  out$layout <- layout
+  out$bg <- bg
+  out$fonts <- fonts
+  out$pointsize <- pointsize
+  out$editable <- editable
+  class(out) <- "dml"
+  return(out)
+}
+
+#' @title Render a plot as a Powerpint DrawingML object
+#' @description Function used to render DrawingML in knitr/rmarkdown documents.
+#' Only Powerpoint outputs currently supported
+#'
+#' @param x a \code{dml} object
+#' @param ... further arguments, not used.
+#' @author Noam Ross
+#' @importFrom knitr knit_print asis_output opts_knit
+#' @importFrom rmarkdown pandoc_version
+#' @importFrom xml2 xml_find_first
+#' @importFrom grDevices dev.off
+#' @export
+knit_print.dml <- function(x, ...) {
+  if (pandoc_version() < 2.4) {
+    stop("pandoc version >= 2.4 required for DrawingML output in pptx")
+  }
+
+  if (is.null(opts_knit$get("rmarkdown.pandoc.to")) ||
+      opts_knit$get("rmarkdown.pandoc.to") != "pptx") {
+    stop("DrawingML currently only supported for pptx output")
+  }
+
+  if (inherits(x$layout, "list")) {
+    id_xfrm <- x$layout
+  } else {
+    id_xfrm <- get_content_layout(x$layout)
+  }
+
+  dml_file <- tempfile(fileext = ".dml")
+  img_directory = get_img_dir()
+
+  dml_pptx(file = dml_file, width = id_xfrm$width, height = id_xfrm$height,
+           offx = id_xfrm$left, offy = id_xfrm$top, pointsize = x$pointsize,
+           last_rel_id = 1L,
+           editable = x$editable, standalone = FALSE, raster_prefix = img_directory)
+
+  tryCatch({
+    if (!is.null(x$ggobj) ) {
+      stopifnot(inherits(x$ggobj, "ggplot"))
+      print(x$ggobj)
+    } else {
+      eval(x$code)
+    }
+  }, finally = dev.off() )
+
+  dml_xml <- read_xml(dml_file)
+  raster_files <- list_raster_files(img_dir = img_directory )
+
+  if (length(raster_files)) {
+    rast_element <- xml_find_all(dml_xml, "//p:pic/p:blipFill/a:blip")
+    raster_files <- list_raster_files(img_dir = img_directory )
+    raster_id <- xml_attr(rast_element, "embed")
+    xml_attr(rast_element, "r:embed") <- raster_files
+  }
+  dml_str <- paste(
+    as.character(xml_find_first(dml_xml, "//p:grpSp")),
+    collapse = "\n"
+  )
+
+  knit_print(asis_output(
+    x = paste("```{=openxml}", dml_str, "```", sep = "\n")
+  ))
+}
+
+
+
+#' If size is not provided, get the size of the main content area of the slide
+#' @noRd
+#' @importFrom officer read_pptx
+get_content_layout_uncached <- function(layout) {
+  ref_pptx <- read_pptx(get_reference_pptx())
+  if (layout == "full_slide") {
+    officer::ph_location_fullsize(ref_pptx)
+  } else if (layout == "default") {
+    officer::ph_location_type(ref_pptx, layout = "Title and Content",
+                              master = "Office Theme", type = "body")
+  } else if (layout == "left_col") {
+    officer::ph_location_left(ref_pptx)
+  } else if (layout == "right_col") {
+    officer::ph_location_right(ref_pptx)
+  } else {
+    stop("Unknown layout type")
+  }
+}
+
+#' @importFrom memoise memoise
+#' @noRd
+get_content_layout <- memoise(get_content_layout_uncached)
+
+
+#' @noRd
+#' @importFrom memoise memoise
+get_reference_pptx <- memoise(officer::get_reference_pptx)
+
+#' @export
+print.dml <- function(x, ...) {
+  cat("DrawingML object with parameters:\n")
+  cat(paste0("  ", names(x), ": ", as.character(x), collapse = "\n"))
+}
+
+get_img_dir <- function(){
+  uid <- basename(tempfile(pattern = ""))
+  img_directory = file.path(getwd(), uid )
+  img_directory
+}
+list_raster_files <- function(img_dir){
+  path_ <- dirname(img_dir)
+  uid <- basename(img_dir)
+  list.files(path = path_, pattern = paste0("^", uid, "(.*)\\.png$"), full.names = TRUE )
+}
+
---
+++ R/rpptx_document.R
@@ -0,0 +1,38 @@
+#' @export
+#' @title Convert to an MS PowerPoint document
+#' @description Format for converting from R Markdown to an MS PowerPoint
+#' document.
+#' @param ... arguments used by \link[rmarkdown]{powerpoint_presentation}
+rpptx_document <- function(...) {
+
+  output_formats <- rmarkdown::powerpoint_presentation(...)
+
+  output_formats$post_processor <- function(metadata, input_file, output_file, clean, verbose) {
+    x <- officer::read_pptx(output_file)
+
+    # iterate over slides - not very nice but no better solution for now
+    for(slide_i in seq_len(length(x)) ){
+      x <- on_slide(x, index = slide_i)
+      slide <- x$slide$get_slide(x$cursor)
+
+      blips <- xml_find_all(slide$get(), "//a:blip[@r:embed]")
+      is_hacked <- grepl( "\\.png$", xml_attr(blips, "embed") )
+      blips <- blips[is_hacked]
+      imgs <- xml_attr(blips, "embed")[is_hacked]
+      for( img in imgs )
+        slide$reference_img(src = img, dir_name = file.path(x$package_dir, "ppt/media"))
+      rel_df <- slide$rel_df()
+      rids <- rel_df$id[match( imgs, rel_df$ext_src)]
+      for( rid in seq_along(rids) ){
+        xml_attr(blips[rid], "r:embed") <- rids[rid]
+      }
+    }
+
+    print(x, target = output_file)
+    output_file
+  }
+
+  output_formats
+}
+
+
