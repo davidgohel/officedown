3f6dd5884bcca519ff6f1a9d60f06e72d8eed51b
David Gohel <david.gohel@ardata.fr>
2020-06-29 17:18:12 +0200

    use officer:::get_default_pandoc_data_file

--- R/rpptx_document.R
+++ R/rpptx_document.R
@@ -53,7 +53,7 @@ rpptx_document <- function(base_format = "rmarkdown::powerpoint_presentation",
 
   args <- list(...)
   if(is.null(args$reference_doc)){
-    reference_doc <- get_default_pandoc_data_file(format = "pptx")
+    reference_doc <- officer:::get_default_pandoc_data_file(format = "pptx")
   } else reference_doc <- args$reference_doc
   new_reference_doc <- tempfile(fileext = ".pptx")
   officer::annotate_base(reference_doc, output_file = new_reference_doc)
@@ -117,25 +117,3 @@ get_pptx_uncached <- function() {
 #' @importFrom memoise memoise
 get_reference_pptx <- memoise(get_pptx_uncached)
 
-#' @importFrom rmarkdown pandoc_available pandoc_exec
-get_default_pandoc_data_file <- function(format = "pptx") {
-  outfile <- tempfile(fileext = paste0(".", format))
-
-  pandoc_exec <- rmarkdown::pandoc_exec()
-  if(.Platform$OS.type %in% "windows"){
-    pandoc_exec <- paste0(pandoc_exec, ".exe")
-  }
-
-  if(!rmarkdown::pandoc_available() || !file.exists(pandoc_exec)){
-    # Use officer template when no pandoc - stupid fallback as if o pandoc, no result
-    outfile <- system.file(package = "officer", "template", paste0("template.", format))
-  } else {
-    # note in that case outfile will be removed when session will end
-    ref_doc <- paste0("reference.", format)
-    system2(pandoc_exec,
-            args = c("--print-default-data-file", ref_doc),
-            stdout = outfile)
-  }
-
-  return(outfile)
-}
