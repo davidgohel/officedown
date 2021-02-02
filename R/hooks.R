c711461ca48d47d5584f0db89be7e75d10a89c31
Martin Camitz <martin.camitz@gmail.com>
2021-02-02 18:57:57 +0100

    Support for reverse order of caption and figure
    Using chunk option fig.cap.below_fig = FALSE (default true), caption appears bove figure in output.

--- R/hooks.R
+++ R/hooks.R
@@ -10,7 +10,8 @@ plot_word_fig_caption <- function(x, options) {
   if(is.null(options$fig.id))
     fig.id <- options$label
   else fig.id <- options$fig.id
-
+  if(!is.logical(options$fig.cap.below_fig)) options$fig.cap.below_fig <- TRUE
+  
   bc <- block_caption(label =  options$fig.cap, style = options$fig.cap.style,
                       autonum = run_autonum(
                         seq_id = gsub(":$", "", options$fig.lp),
@@ -46,5 +47,7 @@ plot_word_fig_caption <- function(x, options) {
   ooxml <- sprintf(ooxml, opts_current$get("fig.align"), fig.style_id)
   img_wml <- paste("```{=openxml}", ooxml, "```", sep = "\n")
 
-  paste("", img_wml, cap_str, sep = "\n\n")
+  ifelse(options$fig.cap.below_fig,
+         paste("", img_wml, cap_str, sep = "\n\n"),
+         paste("", cap_str, img_wml, sep = "\n\n"))
 }
